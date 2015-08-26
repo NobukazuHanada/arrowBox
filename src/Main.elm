72186dde285aaab435865b6d69a917edcc4f5a01
NobukazuHanada <tone.nobukazu@gmail.com>
2015-08-26 12:52:31 +0900

    added Diagram Types

--- src/Main.elm
+++ src/Main.elm
@@ -2,5 +2,26 @@ module Main where
 
 import Html exposing (text)
 
+type Position = { x : Int, y : Int }
+
+
+-- Diagram Data
+type Connection =
+  Apply Box Box
+    | Set Box Box
+    | Solo Box
+
+type Box = IntBox { pos : Posistion, value : Int }
+         | PlusBox { pos : Position }
+         | Varbox String { pos : Position, name : String }
+
+
+type Diagram =
+  Diagram {
+    connections : List Connection,
+    varTable : Dict String Diagram
+  }
+
+
 main =
   text "Hello World"
